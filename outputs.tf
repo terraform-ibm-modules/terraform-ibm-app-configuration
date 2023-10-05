81d8b9404e9dcb9c7044249356d08b524ab12ede
Matthew.Lemmond@ibm.com <Matthew.Lemmond@ibm.com>
2023-10-05 10:22:39 -0400

    feat: initial instance and collection(s) implementation
    Create an instance and optionally create collections based on inputs provided, TODO add documentation and outputs, setup testing, add other app config resources as needed.

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,12 @@
 # Outputs
 ########################################################################################################################
 
-#output "myoutput" {
-#  description = "Description of my output"
-#  value       = "value"
-#}
+output "app_config_guid" {
+  description = "GUID of the App Configuration instance"
+  value       = ibm_resource_instance.app_config.guid
+}
+
+output "app_config_collection_ids" {
+  description = "List of IDs for the collections in the App Configuration instance"
+  value       = [for obj in ibm_app_config_collection.collections : obj.collection_id]
+}
--- variables.tf
+++ variables.tf
@@ -1,9 +1,62 @@
+##############################################################################
+# Common variables
+##############################################################################
+
+variable "resource_group_id" {
+  type        = string
+  description = "The resource group ID where resources will be provisioned."
+}
+
+variable "region" {
+  description = "The region to provision the resources."
+  type        = string
+  default     = "us-south"
+}
+
 ########################################################################################################################
-# Input Variables
+# App Config Instance Variables
 ########################################################################################################################
 
-#variable "my_variable" {
-#  type        = string
-#  description = "A description of my variable"
-#  default     = "default_value"
-#}
+variable "app_config_name" {
+  type        = string
+  description = "Name for the App Configuration service instance"
+}
+
+variable "app_config_plan" {
+  type        = string
+  description = "Plan for the App Configuration service instance"
+  default     = "lite"
+
+  validation {
+    condition     = contains(["lite", "standard", "enterprise"], var.app_config_plan)
+    error_message = "Value for plan must be one of the following: \"lite\", \"standard\", or \"enterprise\"."
+  }
+}
+
+variable "app_config_service_endpoints" {
+  type        = string
+  description = "Service Endpoints for the App Configuration service instance"
+  default     = "public"
+
+  validation {
+    condition     = contains(["public", "private", "public-and-private"], var.app_config_service_endpoints)
+    error_message = "Value for service endpoints must be one of the following: \"public\", \"private\", or \"public-and-private\"."
+  }
+}
+
+variable "app_config_tags" {
+  type        = list(string)
+  description = "Optional list of tags to be added to the App Config instance."
+  default     = []
+}
+
+variable "app_config_collections" {
+  description = "A list of collections to be added to the App Configuration instance"
+  type = list(object({
+    name          = string
+    collection_id = string
+    description   = optional(string, "")
+    tags          = optional(string, "")
+  }))
+  default = []
+}
