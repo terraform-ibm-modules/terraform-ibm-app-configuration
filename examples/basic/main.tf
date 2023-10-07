b35f079858b37f9423a3a2cd3031cfc0bd94cb56
Matthew.Lemmond@ibm.com <Matthew.Lemmond@ibm.com>
2023-10-06 21:56:05 -0400

    refactor: add documentation and examples/tests
    Update documentation to reflect module information, flesh out examples, add validation, update tests

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -20,12 +20,4 @@ module "app_config" {
   region            = var.region
   app_config_name   = "${var.prefix}-app-config"
   app_config_tags   = var.resource_tags
-
-  app_config_collections = [
-    {
-      name          = "${var.prefix}-collection",
-      collection_id = "${var.prefix}-collection"
-      description   = "Collection for ${var.prefix}"
-    }
-  ]
 }
