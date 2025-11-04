7efab4e02cab791dc6fe3140ce047b1b09dc82b5
Harika-Ponna <Harika.Ponna@ibm.com>
2025-11-04 11:39:56 +0530

    feat: Added next step urls for the DA

--- solutions/fully-configurable/outputs.tf
+++ solutions/fully-configurable/outputs.tf
@@ -26,3 +26,18 @@ output "app_config_collection_ids" {
   description = "List of IDs for the collections in the App Configuration instance"
   value       = module.app_config.app_config_collection_ids
 }
+
+output "next_steps_text" {
+  value       = "Your App Config Environment is ready."
+  description = "Next steps text"
+}
+
+output "next_step_primary_label" {
+  value       = "Go to App Config Instance"
+  description = "Primary label"
+}
+
+output "next_step_primary_url" {
+  value       = "https://cloud.ibm.com/services/apprapp/${module.app_config.app_config_crn}?paneId=manage"
+  description = "Primary URL"
+}
