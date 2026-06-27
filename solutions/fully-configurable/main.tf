9cd942cd6fd076335fa03e5cbb324dfba4a80139
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-06-27 08:46:57 +0530

    fix(deps): update dependencies (#387)

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -38,14 +38,14 @@ module "resource_group" {
 module "existing_kms_crn_parser" {
   count   = var.kms_encryption_enabled && var.existing_kms_instance_crn != null ? 1 : 0
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.6.1"
+  version = "1.9.0"
   crn     = var.existing_kms_instance_crn
 }
 
 module "existing_kms_key_crn_parser" {
   count   = var.kms_encryption_enabled && var.existing_kms_key_crn != null ? 1 : 0
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.6.1"
+  version = "1.9.0"
   crn     = var.existing_kms_key_crn
 }
 
@@ -57,7 +57,7 @@ module "existing_kms_key_crn_parser" {
 module "existing_en_crn_parser" {
   count   = var.enable_event_notifications && var.existing_event_notifications_instance_crn != null ? 1 : 0
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.6.1"
+  version = "1.9.0"
   crn     = var.existing_event_notifications_instance_crn
 }
 
