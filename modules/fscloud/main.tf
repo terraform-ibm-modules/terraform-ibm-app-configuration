784df0bfe0a382791f758aac2806252ae53496c6
raagamounica <mounica.tellabati@ibm.com>
2026-08-09 17:31:53 +0530

    fix: fix precommit errors

--- modules/fscloud/main.tf
+++ modules/fscloud/main.tf
@@ -3,30 +3,20 @@
 ##############################################################################
 
 module "app_config" {
-  source = "../../"
-
-  # User-provided required variables
-  resource_group_id = var.resource_group_id
-  region            = var.region
-  app_config_name   = var.app_config_name
-
-  # FSCloud HARDCODED SETTINGS - NON-NEGOTIABLE
-  app_config_plan              = "enterprise"
-  app_config_service_endpoints = "public-and-private"
-  kms_encryption_enabled       = true
-
-  # KMS Configuration (required for FSCloud)
+  source                          = "../../"
+  resource_group_id               = var.resource_group_id
+  region                          = var.region
+  app_config_name                 = var.app_config_name
+  app_config_plan                 = "enterprise"
+  app_config_service_endpoints    = "public-and-private"
+  kms_encryption_enabled          = true
   existing_kms_instance_crn       = var.existing_kms_instance_crn
   root_key_id                     = var.root_key_id
   kms_endpoint_url                = var.kms_endpoint_url
   skip_app_config_kms_auth_policy = var.skip_app_config_kms_auth_policy
-
-  # Context-based restrictions (configurable)
-  cbr_rules = var.cbr_rules
-
-  # Tagging
-  access_tags   = var.access_tags
-  resource_tags = var.resource_tags
+  cbr_rules                       = var.cbr_rules
+  access_tags                     = var.access_tags
+  resource_tags                   = var.resource_tags
 
   # Event Notifications (optional)
   enable_event_notifications                      = var.enable_event_notifications
